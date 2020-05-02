29b59c71c4451cc0efbdd161eaf6c82e6e2e6cdc
David Mašek <masekda4@fit.cvut.cz>
2020-05-02 16:07:59 +0200

    add disable option

--- jupyter_autosave.py
+++ jupyter_autosave.py
@@ -1,8 +1,22 @@
 from shutil import copyfile
+from os import remove
 
 def enable():
     copyfile('jupyter_autosave_hook', '.git/hooks/pre-commit')
     print('Jupyter autosave enabled')
 
+def disable():
+    remove('.git/hooks/pre-commit')
+    print('Jupyter autosave enabled')
+
 if __name__ == "__main__":
-    enable()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('action', choices=['enable', 'disable'])
+    args = parser.parse_args()
+    if args.action == 'enable':
+        enable()    
+    elif args.action == 'disable':
+        disable()
+    else:
+        raise ValueError('invalid action')
