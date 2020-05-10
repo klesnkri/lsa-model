99b36d711b14c33c21afaed02cfc1de3fd7b709c
David Mašek <masekda4@fit.cvut.cz>
2020-05-10 19:39:54 +0200

    nltk dependencies

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export FLASK_APP=server
-python nltk_build.py
-flask update
--- nltk_build.py
+++
@@ -1,5 +0,0 @@
-import nltk
-
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('averaged_perceptron_tagger')
